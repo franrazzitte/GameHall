80d7b9257cd8df4bce0007dd00c5053d39c7b5ff
Francisco Razzitte <98547220+franrazzitte@users.noreply.github.com>
2026-05-18 21:23:38 -0300

    Revert "Sprint #1 - Deploy, Navegación, GitHub API, Favicon"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { Header } from './components/header/header';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Header],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('gamehub');
+}
